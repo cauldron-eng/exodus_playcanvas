36c919f2025092a46802b41e6d13df4e5f99028d
Matthew Newcombe <mnewcombe@cauldron.co>
2022-01-12 10:21:38 +0000

    first commit

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1308575.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 64962266, 64962267, 64962268, 64962269, 65142085, 65142885, 65143226, 65143227 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/64962290/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/64962291/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/64962289/1/ammo.js', 'preload' : true},
+];
